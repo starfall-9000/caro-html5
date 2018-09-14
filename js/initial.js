2ce107146fe7abee41f5abdb06fc1eb4ebfe91f5
starfall_9000 <starfall.9000.21@gmail.com>
2018-09-14 11:56:56 +0700

    integrate fb instant

--- js/initial.js
+++ js/initial.js
@@ -3,6 +3,13 @@ if (Constants.ENV === 'DEV') {
     // app open
     initGame()
   })
+} else {
+  FBInstant.initializeAsync().then(function() {
+    FBInstant.setLoadingProgress(100)
+    FBInstant.startGameAsync().then(function() {
+      initGame()
+    })
+  })
 }
 
 function initGame() {
